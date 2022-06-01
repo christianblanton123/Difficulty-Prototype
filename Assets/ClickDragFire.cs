f956b877f6400dbd67845ad3ad51ada6019092cc
Christian Blanton <cblanton@usc.edu>
2022-06-01 14:59:46 -0700

    trail angry birds shooting

--- Assets/ClickDragFire.cs
+++ Assets/ClickDragFire.cs
@@ -8,14 +8,21 @@ public class ClickDragFire : MonoBehaviour
     Vector3 InitmousePos;
     Vector3 finalMousePos;
     Rigidbody2D rb;
+    LineRenderer arrow;
+    Vector3 zOffset;
+    TrailRenderer tr;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        arrow = GetComponentInChildren<LineRenderer>();
+        zOffset = new Vector3(0, 0, 10);
+        tr = GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetButtonDown("Fire1"))
         {
             InitmousePos = Input.mousePosition;
@@ -24,7 +31,28 @@ public class ClickDragFire : MonoBehaviour
         {
             finalMousePos = Input.mousePosition;
             //shoot
-            rb.AddForce(new Vector2(InitmousePos.x,InitmousePos.y)-new Vector2(finalMousePos.x,finalMousePos.y));
+            tr.Clear();
+            rb.AddForce((new Vector2(InitmousePos.x, InitmousePos.y) - new Vector2(finalMousePos.x, finalMousePos.y))*4);
+        }
+        else if (Input.GetButton("Fire1"))
+        {
+            arrow.SetPosition(0, rb.transform.position);
+            arrow.SetPosition(1, rb.transform.position+Camera.main.ScreenToWorldPoint(InitmousePos)-(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+
+            // A simple 2 color gradient with a fixed alpha of 1.0f.
+            float alpha = 1.0f;
+            float dist = Vector2.Distance(new Vector2(arrow.GetPosition(0).x, arrow.GetPosition(0).y), new Vector2(arrow.GetPosition(1).x, arrow.GetPosition(1).y));
+            float lerpFactor = Mathf.Clamp(dist/6, 0, 1);
+            Debug.Log("Lerp" + lerpFactor);
+            Gradient gradient = new Gradient();
+            Color lerpedColor = Color.Lerp(Color.green, Color.red, lerpFactor);
+            gradient.SetKeys(
+                new GradientColorKey[] { new GradientColorKey(Color.green, 0.0f), new GradientColorKey(lerpedColor, 1.0f) },
+                new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+            );
+            arrow.colorGradient = gradient;
+
+            
         }
     }
 }
