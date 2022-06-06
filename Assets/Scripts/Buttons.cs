bf313ce28712618d8dcbd57ef44ba3c8387231fc
Christian Blanton <cblanton@usc.edu>
2022-06-06 16:43:19 -0700

    buttons

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
@@ -10,7 +11,12 @@ public class Buttons : MonoBehaviour
     private bool hasAnimation;
     private bool hasScreenShake;
     private bool hasColor;
-
+    [SerializeField] private Button particles;
+    [SerializeField] private Button audio;
+    [SerializeField] private Button animation;
+    [SerializeField] private Button screenShake;
+    [SerializeField] private Button colorButton;
+    
 
     public Buttons() {
 
@@ -24,6 +30,7 @@ public class Buttons : MonoBehaviour
         hasAnimation = false;
         hasScreenShake = false;
         hasColor = false;
+        
     }
 
     // Update is called once per frame
@@ -54,28 +61,41 @@ public class Buttons : MonoBehaviour
     public void changeHasAnimation()
     {
         hasAnimation = !hasAnimation;
+        if (hasAnimation)
+        {
+            animation.GetComponent<Image>().color = Color.green;
+        }
+        else
+            animation.GetComponent<Image>().color = Color.white;
+
     }
     public void changeHasScreenShakeState() {
         if (!getHasScreenShake()) {
             hasScreenShake = true;
+            screenShake.GetComponent<Image>().color = Color.green;
         } else {
             hasScreenShake = false;
+            screenShake.GetComponent<Image>().color = Color.white;
         }
     }
 
     public void changeHasParticles() {
         if (!getHasParticles()) {
             hasParticles = true;
+            particles.GetComponent<Image>().color = Color.green;
         } else {
             hasParticles = false;
+            particles.GetComponent<Image>().color = Color.white;
         }
     }
 
     public void changeHasColor() {
         if (!getHasColor()) {
             hasColor = true;
+            colorButton.GetComponent<Image>().color = Color.green;
         } else {
             hasColor = false;
+            colorButton.GetComponent<Image>().color = Color.white;
         }
     }
 
@@ -84,9 +104,11 @@ public class Buttons : MonoBehaviour
             hasAudio = true;
             FindObjectOfType<AudioManager>().ReturnVolume("Music");
             FindObjectOfType<AudioManager>().swishNoise();
+            audio.GetComponent<Image>().color = Color.green;
         } else {
             hasAudio = false;
             FindObjectOfType<AudioManager>().NoVolume("Music");
+            audio.GetComponent<Image>().color = Color.white;
         }
     }
 
